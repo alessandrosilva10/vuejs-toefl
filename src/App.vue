6f200374b0fb417c62f5be3a2f70babf233a2f62
Alessandro <alessandro.mpgh@gmail.com>
2021-02-27 11:16:16 -0300

    Blocked mobile access

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
 <v-app style="margin-left: -20px; margin-right: -20px;">
+        <div class="hidden-md-and-up">
+    <v-card class="mt-5 mb-8">
+      <v-card-text>
+        <v-container>
+                <v-card  color="blue lighten-3" dark>
+                  <v-card-title></v-card-title>
+                  <v-card-text>
+                      <img src="https://freepngimg.com/thumb/blocked/3-2-blocked-png-picture.png" />
+                      <h1 class="white--text center">Sorry - we currently do not support mobile access.</h1>
+                  </v-card-text>
+                </v-card>
+        </v-container>
+      </v-card-text>
+    </v-card>
+</div>
  <v-content class="mx-4">
     <router-view></router-view>
      </v-content>
-    </v-app>
+</v-app>
 </template>
 
 <script>
