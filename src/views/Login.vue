7a5e6593501c17d9575487bc50afc6f3a85de6eb
Alessandro <alessandro.mpgh@gmail.com>
2021-02-27 03:59:53 -0300

    recaptcha v3

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -99,7 +99,7 @@ import { VueReCaptcha } from 'vue-recaptcha-v3'
 // For more options see below
 Vue.use(VueReCaptcha, { siteKey: '6LcR9WkaAAAAAOmFZF2P26dHXEQkb2JKNMD20-bz', loaderOptions: {
     useRecaptchaNet: true,
-    autoHideBadge: autoHideBadge
+    autoHideBadge: this.autoHideBadge
   }})
 // secret key: 6LcR9WkaAAAAAEdbIo6Wu_TbPV4rbGAtLhKDb_c7
 
